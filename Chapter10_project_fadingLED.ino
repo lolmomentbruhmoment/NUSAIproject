7ac98f0c352b248f90f842a60f41555244d5a502
cooljiho <90885592+lolmomentbruhmoment@users.noreply.github.com>
2023-06-21 10:27:14 +0800

    Update Chapter10_project_fadingLED.ino

--- Chapter10_project_fadingLED.ino
+++ Chapter10_project_fadingLED.ino
@@ -11,6 +11,9 @@
 float smoothness_pts = 1000;
 int x = 0;
 
+float gammaC = 0.14;
+float betaC = 0.5;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -21,8 +24,15 @@ void setup() {
 void loop() {
     if (x >= smoothness_pts)
         x = 0;
-     
+    //Simple triangle breathing LED on the PICO-W board
+    float pwm_val = 255.0*(1.0 -  abs((2.0*(x/smoothness_pts))-1.0));
+
+    //Circular Wave breathing LED on the PICO-W board
     float pwm_val = 255.0*sqrt(1.0 -  pow(abs((2.0*(x/smoothness_pts))-1.0),2.0));
+
+    //Gausian Functions breathing LED on the PICO-W board
+    float pwm_val = 255.0*(exp(-(pow(((x/smoothness_pts)-betaC)/gammaC,2.0))/2.0));
+
     analogWrite(ONBOARD_LED,int(pwm_val));
     x += 1;
     delay(5);                    
